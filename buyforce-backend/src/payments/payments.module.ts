a9a82768aad14d308dac16ff67bc6495ad42844e
tehila.ch <tehila.ch12@gmail.com>
2025-12-18 23:10:10 +0200

    Update payments.module.ts

--- buyforce-backend/src/payments/payments.module.ts
+++ buyforce-backend/src/payments/payments.module.ts
@@ -3,7 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { PaypalService } from './paypal.service';
-import { getPayPalClient } from './paypal.client';
+
 @Module({
   controllers: [PaymentsController],
   providers: [PrismaService, PaymentsService, PaypalService],
